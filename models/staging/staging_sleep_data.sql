c0ec8f10aad28524122cc2a5f8753aa1841bd66d
Blaise Clarke <bc@blaiseclarke.com>
2025-11-21 21:32:19 +0000

    Added necessary package (dbt-utils) and successfully cleaned warehouse data

---
+++ models/staging/staging_sleep_data.sql
@@ -0,0 +1,24 @@
+with source as (
+    select * from {{ source('eeg_raw', 'SLEEP_EPOCHS') }}
+),
+
+cleaned as (
+    select
+        -- Generate unique ID
+        {{ dbt_utils.generate_surrogate_key(['subject_id', 'epoch_idx']) }} as epoch_id,
+
+        cast(subject_id as varchar) as subject_id,
+        cast(epoch_idx as int) as epoch_idx,
+        cast(stage as varchar) as sleep_stage,
+        
+        -- Rename the power to include units (uv)
+        cast(delta_power as float) as delta_power_uv,
+        cast(theta_power as float) as theta_power_uv,
+        cast(alpha_power as float) as alpha_power_uv,
+        cast(sigma_power as float) as sigma_power_uv,
+        cast(beta_power as float) as beta_power_uv
+
+    from source
+)
+
+select * from cleaned
