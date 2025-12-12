eda02a0482de0082d7b573b3ee5791a3ec282a87
Blaise Clarke <blaiseclarke@fastmail.com>
2025-12-12 09:22:44 -0500

    Fixed bug that was computing PSD from one EEG channel only

--- ingest_data.py
+++ ingest_data.py
@@ -95,7 +95,7 @@ def process_subject(subject_id):
 def calculate_band_power(psd, frequencies, fmin, fmax):
         idx = np.logical_and(frequencies >= fmin, frequencies <= fmax)
 
-        power = psd[:, 0, idx].mean(axis=1) * 1e12
+        power = psd[:, :, idx].mean(axis=1) * 1e12
         return power
 
 def main():
